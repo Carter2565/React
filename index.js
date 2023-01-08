8b38d52f2d442c82d085a74139b49d97253bc631
Carter2565 <94093508+Carter2565@users.noreply.github.com>
2023-01-08 10:48:07 +0000

    timer update

--- index.js
+++ index.js
@@ -99,7 +99,6 @@ function loaded() {
   }
   
   document.getElementById('button').onpointerdown = function() {
-    wait = 1;
     delay = Math.random() * (2.5 - 1.25) + 1.25;
     setTimeout(function() {
       if (wait = 1) { 
