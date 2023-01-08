99bec6181a70dc9ba4fe82be19b8b8aaa6607daf
Carter2565 <94093508+Carter2565@users.noreply.github.com>
2023-01-08 04:40:56 +0000

    js start

---
+++ index.js
@@ -0,0 +1,41 @@
+function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    /* Loop through a collection of all HTML elements: */
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+      elmnt = z[i];
+      /*search for elements with a certain atrribute:*/
+      file = elmnt.getAttribute("include-html");
+      if (file) {
+        /* Make an HTTP request using the attribute value as the file name: */
+        xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4) {
+            if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+            if (this.status == 404) {elmnt.innerHTML = "404 - Page not found.";}
+            /* Remove the attribute, and call this function once more: */
+            elmnt.removeAttribute("include-html");
+            includeHTML();
+          }
+        }
+        xhttp.open("GET", file, true);
+        xhttp.send();
+        /* Exit the function: */
+        return;
+      }
+    }
+  }
+function clearIncluded() {
+  let y = document.getElementsByClassName("included");
+  y = y.length -1
+  for (let x = y; x > 0; x--) {
+    document.getElementsByClassName("included")[x].remove();
+  }
+}
+
+function include(file, path="/assets/php/",element="content" ) {
+  let location = path + file;
+  document.getElementById(element).setAttribute("include-html", location);
+  includeHTML();
+}
+
