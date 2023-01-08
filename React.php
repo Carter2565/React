28a78e7ebf3e140bd1b5e7b6227311df4fd5c747
Carter2565 <94093508+Carter2565@users.noreply.github.com>
2023-01-08 04:08:55 +0000

    Create React.php

---
+++ React.php
@@ -0,0 +1,22 @@
+<html>
+  <head>
+    <style>
+      .dot {
+        width: 10px;
+        height: 10px;
+        background-color: black;
+        border-radius: 50%;
+        display: inline-block;
+      }
+    </style>
+  </head>
+  <body>
+    <div>
+      <div class="dot"></div>
+      <div class="dot"></div>
+      <div class="dot"></div>
+      <div class="dot"></div>
+      <div class="dot"></div>
+    </div>
+  </body>
+</html>
