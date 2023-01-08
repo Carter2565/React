d86279b123ea0ef0654973fa914e978d8714fe0b
Carter2565 <94093508+Carter2565@users.noreply.github.com>
2023-01-08 04:42:57 +0000

    updates

--- React.php
+++ React.php
@@ -1,16 +1,12 @@
-<html>
-  <head>
-    <link href="react.css" rel="stylesheet">
-  </head>
-  <body>
-    <div class="dots">
-      <div class="dot"></div>
-      <div class="dot"></div>
-      <div class="dot"></div>
-      <div class="dot"></div>
-      <div class="dot"></div>
-    </div>
-  </body>
-</html>
+<div class='react'>
+  <link href="react.css" rel="stylesheet">
+  <div class="dots">
+    <div class="dot"></div>
+    <div class="dot"></div>
+    <div class="dot"></div>
+    <div class="dot"></div>
+    <div class="dot"></div>
+  </div>
+</div>
 
 
--- index.php
+++ index.php
@@ -2,14 +2,11 @@
   <head>
     <title id='title'>React - Games -Carter2565</title>
     <link rel="icon" type="image/x-icon" href="/assets/icons/favicon.png">
-    <link rel="stylesheet" href="/assets/css/index.css">
-    <script src="/assets/javascript/index.js" ></script>
+    <script src="index.js" ></script>
     <script src="https://kit.fontawesome.com/f048130949.js" crossorigin="anonymous"></script>
   </head>
   <body>
-
     <div id="content" class='included' include-html="React.php"></div>
-    
     <script>
       includeHTML()
     </script>
